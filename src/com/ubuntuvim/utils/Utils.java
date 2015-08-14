21572a7d88beba418de26d37cc0e0604171acc26
ubuntuvim <1527254027@qq.com>
2015-08-14 15:03:06 +0800

    UpdateBySql method to modify the transaction problem ...

---
+++ src/com/ubuntuvim/utils/Utils.java
@@ -0,0 +1,30 @@
+package com.ubuntuvim.utils;
+
+public class Utils {
+	
+	/**
+	 * 首字母大写
+	 * @param c 被转换的字符串
+	 * @return 首字母大写后的字符串
+	 */
+	public static String toUpperCase(String c) {
+		char[] cs = c.toCharArray();
+		cs[0] -= 32;  //第一个字符转为大写
+		return String.valueOf(cs);
+	}
+	
+	/**
+	 * 删除最后一个指定的字符
+	 * @param s
+	 * @return
+	 */
+	public static String removeLastChar(String s, String removeChar) {
+		String tmp = s;
+		if (null != s && !"".equals(s) 
+			&& s.substring(s.length() - 1, s.length()).equals(removeChar)) {
+			return tmp.substring(0, tmp.length() - 1);
+		}
+		
+		return "";
+	}
+}
